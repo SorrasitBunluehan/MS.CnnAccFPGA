8bbf3dccbcd0abca4904a5c5cfd23a9435835772
Sorrasit Bunluehan <b.sorrasit@gmail.com>
2020-11-23 19:25:30 +0100

    Reorganize the folder and files

--- Vitis_workspace/tcl/script.tcl
+++
@@ -1,7 +0,0 @@
-#Create Platform Project and set Active
-platform create -name {Hw2WithAcc} -hw {D:\Vivado_Project\dma_test\design_1_wrapper.xsa} -proc {ps7_cortexa9_0} -os {standalone} -out {D:/Vivado_Project/dma_test/vitis};platform write
-platform read {D:\Vivado_Project\dma_test\vitis\Hw2WithAcc\platform.spr}
-platform active {Hw2WithAcc}
-
-#Build a Platform Project
-
